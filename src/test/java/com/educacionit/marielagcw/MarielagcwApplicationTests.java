e7179d6d8f0cac017abd5123f20709167c95b90a
Marielagcw <marielagcw@hotmail.com>
2023-05-29 00:18:50 -0300

    Agregado tests unitarios

--- src/test/java/com/educacionit/marielagcw/MarielagcwApplicationTests.java
+++ src/test/java/com/educacionit/marielagcw/MarielagcwApplicationTests.java
@@ -1,5 +1,7 @@
 package com.educacionit.marielagcw;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,28 @@ class MarielagcwApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	// Pruebas unitarias
+
+	@Test
+	void prueba1() {
+		assertEquals(true, false); // Rojo
+	}
+
+	@Test
+	void prueba2() {
+		assertEquals(true, true); // Verde
+	}
+
+	@Test
+	void prueba3() {
+		assertEquals(2+2, 4); // Verde
+	}
+
+	@Test
+	void prueba4() {
+		assertEquals(2+3, 4); // Rojo
 	}
 
 }
